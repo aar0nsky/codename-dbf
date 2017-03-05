16e52a500996872cc5c8463a1f7452bd059631b6
Charlie Winnard <ctwinnard@gmail.com>
2017-03-04 23:06:30 -0500

    Added a fizz buzz landing page with form that allows for control of numberOfInputs passed to FizzBuzzController

--- src/main/java/com/dbf/fizzbuzz/FizzBuzzController.java
+++ src/main/java/com/dbf/fizzbuzz/FizzBuzzController.java
@@ -1,4 +1,4 @@
-package com.dbf.controller;
+package com.dbf.fizzbuzz;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +12,17 @@ public class FizzBuzzController {
 	
 	FizzBuzzService fizzBuzzService = new FizzBuzzService();
 	
+	@RequestMapping("/fizzbuzz-landing")
+    public String landing() {
+        return "fizzbuzz/fizzbuzz-landing";
+    }
+	
 	@RequestMapping("/fizzbuzz")
-    public String greeting(@RequestParam(value="num", required=false, defaultValue="15") String numberOfItems, Model model) {
+    public String fizzBuzz(@RequestParam(value="num", required=false, defaultValue="15") String numberOfItems, Model model) {
 		String fizzBuzzString = this.fizzBuzzService.fizzBuzz(Integer.parseInt(numberOfItems));
 		
 		model.addAttribute("fizzBuzzString", fizzBuzzString);
 		
-        return "fizzbuzz";
+        return "fizzbuzz/fizzbuzz-results";
     }
 }
